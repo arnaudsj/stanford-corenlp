4099e3f51ee48f7325a46443803d8b3dbdf5ee8d
Hans Engel <engel@engel.uk.to>
2014-07-18 11:11:25 -0700

    Update Clojure and CoreNLP dependencies to latest stable versions

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject es.corygil/corenlp "3.3.1-SNAPSHOT"
+(defproject es.corygil/corenlp "3.4"
   :description "Clojure wrapper for the Stanford CoreNLP tools."
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [edu.stanford.nlp/stanford-corenlp "3.3.1"]
-                 [edu.stanford.nlp/stanford-corenlp "3.3.1"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [edu.stanford.nlp/stanford-corenlp "3.4"]
+                 [edu.stanford.nlp/stanford-corenlp "3.4"
                   :classifier "models"]
                  [cc.artifice/loom "0.1.3"]]
   :plugins [[lein-exec "0.3.2"]])
