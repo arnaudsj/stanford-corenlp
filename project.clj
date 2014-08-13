17fc02443eb3a1735a7647a1f81f298fa11b114a
Sébastien Arnaud <arnaudsj@gmail.com>
2014-08-12 21:05:24 -0400

    Added license, url, scm, org id for clojars

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
-(defproject es.corygil/corenlp "3.4"
+(defproject me.arnaudsj/corenlp "3.4"
   :description "Clojure wrapper for the Stanford CoreNLP tools."
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [edu.stanford.nlp/stanford-corenlp "3.4"]
                  [edu.stanford.nlp/stanford-corenlp "3.4"
                   :classifier "models"]
                  [cc.artifice/loom "0.1.3"]]
-  :plugins [[lein-exec "0.3.2"]])
+  :plugins [[lein-exec "0.3.2"]]
+  :url "https://github.com/arnaudsj/stanford-corenlp"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+         :url "https://github.com/arnaudsj/stanford-corenlp"})
