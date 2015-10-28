12376a039e925f59ae4118fd75fc5af26ca1e4eb
keorn <pczaban@gmail.com>
2015-10-28 16:31:13 +0000

    make dependency graph work, with text that contains non-word signs

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [edu.stanford.nlp/stanford-corenlp "3.5.2"]
                  [edu.stanford.nlp/stanford-corenlp "3.5.2" :classifier "models"]
-                 [cc.artifice/loom "0.1.3"]]
-  :plugins [[lein-exec "0.3.5"]]
+                 [aysylu/loom  "0.5.4"]]
+  :plugins [[lein-exec "0.3.5"]
+            [lein-gorilla  "0.3.4"]]
   :url "https://github.com/arnaudsj/stanford-corenlp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
--- test/corenlp/parsing_test.clj
+++ test/corenlp/parsing_test.clj
@@ -1,6 +1,7 @@
 (ns corenlp.parsing-test
   (:use clojure.test)
-  (:require [corenlp]))
+  (:require [corenlp]
+            [loom.graph :as lg]))
 
 
 
@@ -26,3 +27,9 @@
   "Named Entity Recognition"
   (is (= '("PERSON" "PERSON" "O" "O" "O" "O" "O" "LOCATION" "LOCATION" "O")
          (corenlp/named-entities "Barack Obama is the president of the United States."))))
+
+(deftest dependency-graph-test
+  "Loom dependency graph"
+  (let [dp (corenlp/dependency-parse (corenlp/tokenize "Me, and you, like cheese (a lot)."))]
+    (is (= #{0 -1 6 3 2 9 5 8}
+           (lg/nodes (corenlp/dependency-graph dp))))))
